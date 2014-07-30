e0af899e7d7eec2382783370fa20b3e367dfb2f3
Markus Makela <markus.makela@skysql.com>
2014-07-30 12:48:57 +0300

    added a configuration file for server options

--- rabbitmq_consumer/consumer.c
+++ rabbitmq_consumer/consumer.c
@@ -2,12 +2,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <ini.h>
 #include <stdint.h>
 #include <amqp_tcp_socket.h>
 #include <amqp.h>
 #include <amqp_framing.h>
 
+typedef struct consumer_t
+{
+  char *hostname,*vhost,*user,*passwd,*queue,*dbserver,*dbname,*dbuser,*dbpasswd;
+  int port,dbport;
+}CONSUMER;
+
+static CONSUMER* c_inst;
+
+int handler(void* user, const char* section, const char* name,
+	    const char* value)
+{
+  if(strcmp(section,"consumer") == 0){
+    
+    if(strcmp(name,"hostname") == 0){
+      c_inst->hostname = strdup(value);
+    }else if(strcmp(name,"vhost") == 0){
+      c_inst->vhost = strdup(value);
+    }else if(strcmp(name,"port") == 0){
+      c_inst->port = atoi(value);
+    }else if(strcmp(name,"user") == 0){
+      c_inst->user = strdup(value);
+    }else if(strcmp(name,"passwd") == 0){
+      c_inst->passwd = strdup(value);
+    }else if(strcmp(name,"queue") == 0){
+      c_inst->queue = strdup(value);
+    }else if(strcmp(name,"dbserver") == 0){
+      c_inst->dbserver = strdup(value);
+    }else if(strcmp(name,"dbport") == 0){
+      c_inst->dbport = atoi(value);
+    }else if(strcmp(name,"dbname") == 0){
+      c_inst->dbname = strdup(value);
+    }else if(strcmp(name,"dbuser") == 0){
+      c_inst->dbuser = strdup(value);
+    }else if(strcmp(name,"dbpasswd") == 0){
+      c_inst->dbpasswd = strdup(value);
+    }
+
+  }
+
+  return 1;
+}
 int isPair(amqp_message_t* a, amqp_message_t* b)
 {
   int keylen = a->properties.correlation_id.len >=
@@ -48,8 +89,8 @@ int sendToServer(amqp_message_t* a, amqp_message_t* b){
 }
 int main(int argc, char** argv)
 {
-  char *hostname,*queue;
-  int port, channel = 1, all_ok = 1, have_q = 0, have_r = 0, status = AMQP_STATUS_OK;
+  const char* fname = "consumer.cnf";
+  int channel = 1, all_ok = 1, have_q = 0, have_r = 0, status = AMQP_STATUS_OK;
   amqp_socket_t *socket = NULL;
   amqp_connection_state_t conn;
   amqp_rpc_reply_t ret;
@@ -57,31 +98,44 @@ int main(int argc, char** argv)
   amqp_frame_t frame;
   struct timeval timeout;
 
-  if (argc < 4) {
-    fprintf(stderr, "Usage: consumer host port queue\n");
-    return 1;
-  }
-  hostname = strdup(argv[1]);
-  port = atoi(argv[2]);
-  queue = strdup(argv[3]);
+  
   timeout.tv_sec = 5;
   timeout.tv_usec = 0;
 
+  if((c_inst = calloc(1,sizeof(CONSUMER))) == NULL){
+    fprintf(stderr, "Fatal Error: Cannot allocate enough memory.");
+    return 1;
+  }
+
+  /**Parse the INI file*/
+  if(ini_parse(fname,handler,NULL) < 0){
+    fprintf(stderr, "Fatal Error: Error parsing configuration file!\n");
+    goto fatal_error;
+  }
+
+  /**Confirm that all parameters were in the configuration file*/
+  if(!c_inst->hostname||!c_inst->vhost||!c_inst->user||
+     !c_inst->passwd||!c_inst->dbpasswd||!c_inst->queue||
+     !c_inst->dbserver||!c_inst->dbname||!c_inst->dbuser){
+    fprintf(stderr, "Fatal Error: Inadequate configuration file!\n");
+    goto fatal_error;    
+  }
+  
   if((conn = amqp_new_connection()) == NULL || 
      (socket = amqp_tcp_socket_new(conn)) == NULL){
-    printf("Fatal Error: Cannot create connection object or socket.");
+    fprintf(stderr, "Fatal Error: Cannot create connection object or socket.");
     goto fatal_error;
   }
   
-  if(amqp_socket_open(socket, hostname, port)){
-    printf("Error: Cannot open socket.");
+  if(amqp_socket_open(socket, c_inst->hostname, c_inst->port)){
+    fprintf(stderr, "Error: Cannot open socket.");
     goto error;
   }
   
-  ret = amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
+  ret = amqp_login(conn, c_inst->vhost, 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, c_inst->user, c_inst->passwd);
 
   if(ret.reply_type != AMQP_RESPONSE_NORMAL){
-    printf("Error: Cannot login to server.\n");
+    fprintf(stderr, "Error: Cannot login to server.\n");
     goto error;
   }
 
@@ -89,17 +143,17 @@ int main(int argc, char** argv)
   ret = amqp_get_rpc_reply(conn);
 
   if(ret.reply_type != AMQP_RESPONSE_NORMAL){
-    printf("Error: Cannot open channel.\n");
+    fprintf(stderr, "Error: Cannot open channel.\n");
     goto error;
   }  
 
   query = malloc(sizeof(amqp_message_t));
   reply = malloc(sizeof(amqp_message_t));
   if(!query || !reply){
-    printf("Error: Cannot allocate enough memory.");
+    fprintf(stderr, "Error: Cannot allocate enough memory.");
     goto error;
   }
-  amqp_basic_consume(conn,channel,amqp_cstring_bytes(queue),amqp_empty_bytes,0,0,0,amqp_empty_table);
+  amqp_basic_consume(conn,channel,amqp_cstring_bytes(c_inst->queue),amqp_empty_bytes,0,0,0,amqp_empty_table);
 
   while(all_ok){
       
@@ -174,7 +228,17 @@ int main(int argc, char** argv)
   amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
   amqp_destroy_connection(conn);
  fatal_error:
-  free(hostname);
-  free(queue);
+  if(c_inst){
+    free(c_inst->hostname);
+    free(c_inst->queue);
+    free(c_inst->dbserver);
+    free(c_inst->dbname);
+    free(c_inst->dbuser);
+    free(c_inst->dbpasswd);
+    free(c_inst);
+
+  }
+
+
   return 1;
 }
